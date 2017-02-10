52a34203c7f6e8c8db23fb3073fd3e9442d5300e
alicialundberg <sara.alicia.lundberg@gmail.com>
2017-02-10 13:56:24 +0000

    Added database dump

---
+++ webshop.sql
@@ -0,0 +1,86 @@
+-- MySQL dump 10.13  Distrib 5.5.54, for debian-linux-gnu (armv7l)
+--
+-- Host: 0.0.0.0    Database: webshop
+-- ------------------------------------------------------
+-- Server version	5.5.54-0+deb8u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Customer`
+--
+
+DROP TABLE IF EXISTS `Customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Customer` (
+  `customerID` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` char(30) NOT NULL,
+  `lastname` varchar(40) NOT NULL,
+  `ssn` varchar(15) NOT NULL,
+  `adress` varchar(50) NOT NULL,
+  `city` char(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(25) NOT NULL,
+  PRIMARY KEY (`customerID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Customer`
+--
+
+LOCK TABLES `Customer` WRITE;
+/*!40000 ALTER TABLE `Customer` DISABLE KEYS */;
+INSERT INTO `Customer` VALUES (1,'Sven','Andersson','19571017-7425','testbacken 2','testeborg','sven.tester@helloworld.com','0700001334'),(2,'Anna','Svensson','19850427-7425','testgatan 7','testköping','annatest@helloworld.com','0704572157'),(3,'Kristian','Testström','19120427-7136','testvägen 33','testeå','Krilletest@helloworld.com','07045743673'),(4,'Elsa','Testsson','19781201-5761','teststigen 4','testholm','testerElsa@helloworld.com','0706971482'),(5,'Julia','Testingsson','19920211-4247','testbacken 8','testeborg','juliatesting@helloworld.com','07069831486');
+/*!40000 ALTER TABLE `Customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Product`
+--
+
+DROP TABLE IF EXISTS `Product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Product` (
+  `productID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `imgname` varchar(50) NOT NULL,
+  `manufacturer` varchar(50) NOT NULL,
+  `instock` varchar(20) NOT NULL,
+  `cost` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`productID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Product`
+--
+
+LOCK TABLES `Product` WRITE;
+/*!40000 ALTER TABLE `Product` DISABLE KEYS */;
+INSERT INTO `Product` VALUES (1,'Volvo','A retro Volvo Amazon -69. Still in mint condition!','volvo.jpg','Volvo','1',1000.00),(2,'SAAB','A retro SAAB that needs some tender loving care.','saab.jpg','SAAB','1',10.00),(3,'iPhone','A defective iPhone in need of repair.','iphone.jpg','Apple','1000',250.00),(4,'Boat','A boat in good condition and still floats!','boat.jpg','Unknown','1',1500.00),(5,'Book','Needful Things by Stephen King. Brand new!','2.jpg','Stephen King','3',300.00),(6,'Paper bag','Needful Things branded Paper bag.','1.png','Unknown','100000',100.00);
+/*!40000 ALTER TABLE `Product` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-10 13:40:39
